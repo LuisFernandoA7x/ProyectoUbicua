8c71dd7004d82e8e1844aaf18b2fae66ae8d166b
Luis Fernando Moreno Vega <42383823+LuisFernandoA7x@users.noreply.github.com>
2021-01-02 20:49:25 -0600

    Pantalla

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const kTextColor = Color(0xFF535353);
+const kTextLightColor = Color(0xFFACACAC);
+
+const kDefaultPaddin = 20.0;
+
 const bgBlueGradient = LinearGradient(
   begin: Alignment.topCenter,
   end: Alignment.bottomCenter,
